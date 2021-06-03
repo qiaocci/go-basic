b10009dd00c99071bb3d4ed08c5061af414d8583
qiaocc <qiaocco@gmail.com>
2021-06-03 10:19:32 +0800

    竞态问题

---
+++ 12-并发编程/11-竞态问题.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg3 sync.WaitGroup
+var x int
+
+func add() {
+	for i := 0; i < 5000; i++ {
+		x = x + i
+		time.Sleep(time.Millisecond * 1)
+	}
+	wg3.Done()
+}
+
+func main() {
+	wg3.Add(2)
+	go add()
+	go add()
+	wg3.Wait()
+	fmt.Println(x) // 正确值： 24995000， 但是打印的值有可能不对。
+}
