0573db2608604c1b16f2f725a641bae3f1db9893
qiaocc <qiaocco@gmail.com>
2021-06-07 16:25:02 +0800

    protobuf教程

---
+++ sec5-protobuf/01-初识/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+	"protobuf01/std"
+)
+
+func main() {
+	student := &std.Student{
+		Name:   "qiaocc",
+		Male:   true,
+		Scores: []int32{98, 97, 96},
+	}
+	data, err := proto.Marshal(student)
+	if err != nil {
+		log.Fatalf("Marshal failed, err:%v\n", err)
+	}
+
+	newStudent := &std.Student{}
+	err = proto.Unmarshal(data, newStudent)
+	if err != nil {
+		log.Fatalf("UnMarshal failed, err:%v\n", err)
+	}
+	fmt.Println(newStudent.Name, newStudent.Male, newStudent.Scores)
+}
