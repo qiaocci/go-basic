f773556759dfd565b7e403eac513b6f5f9309f24
qiaocc <qiaocco@gmail.com>
2021-07-17 23:40:37 +0800

    同时提供http接口

--- sec6-rpc/11-同时提供http接口/client/client.go
+++ sec6-rpc/11-同时提供http接口/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"grpc-client-and-server/pkg"
 	pb "grpc-client-and-server/proto"
 	"log"
 )
@@ -10,9 +11,18 @@ import (
 const PORT = "9001"
 
 func main() {
-	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	tlsClient := pkg.Client{
+		ServerName: "go-grpc-example",
+		CertFile:   "../conf/server/server.pem",
+	}
+	c, err := tlsClient.GetTLSCredentials()
 	if err != nil {
-		log.Fatalf("grpc.Dial err: %v\n", err)
+		log.Fatalf("tlsClient.GetTLSCredentials err: %v", err)
+	}
+
+	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
+	if err != nil {
+		log.Fatalf("grpc.Dial err: %v", err)
 	}
 	defer conn.Close()
 
--- sec6-rpc/11-同时提供http接口/server/server.go
+++ sec6-rpc/11-同时提供http接口/server/server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"grpc-client-and-server/pkg"
 	pb "grpc-client-and-server/proto"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -18,19 +20,33 @@ func (s SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.Sea
 const PORT = "9001"
 
 func main() {
+	certFile := "../conf/server/server.pem"
+	keyFile := "../conf/server/server.key"
+	tlsServer := pkg.Server{
+		CertFile: certFile,
+		KeyFile:  keyFile,
+	}
+
+	c, err := tlsServer.GetTLSCredentials()
+	if err != nil {
+		log.Fatalf("tlsServer.GetTLSCredentials err: %v", err)
+	}
+
 	mux := GetHTTPServeMux()
 
-	server := grpc.NewServer()
+	server := grpc.NewServer(grpc.Creds(c))
 	pb.RegisterSearchServiceServer(server, &SearchService{})
 
-	http.ListenAndServe(":"+PORT, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			server.ServeHTTP(w, r)
-		} else {
-			mux.ServeHTTP(w, r)
-		}
-
-	}))
+	http.ListenAndServeTLS(":"+PORT,
+		certFile,
+		keyFile,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+				server.ServeHTTP(w, r)
+			} else {
+				mux.ServeHTTP(w, r)
+			}
+		}))
 }
 
 func GetHTTPServeMux() *http.ServeMux {
