78b8043736f5b5e348bad4b529ef6617eaefab03
qiaocc <qiaocco@gmail.com>
2021-12-03 17:41:49 +0800

    html模板

---
+++ sec2-标准库/3-html模板/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.64s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string    // in Markdown format
+}
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+func main() {
+	//textTest()
+	templateTest()
+}
+
+func templateTest() {
+	issueList, err := template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+  <th>#</th>
+  <th>State</th>
+  <th>User</th>
+  <th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+  <td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+  <td>{{.State}}</td>
+  <td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+  <td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := SearchIssues([]string{"python", "中文"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = issueList.Execute(os.Stdout, res); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func textTest() {
+	report, err := template.New("report").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := SearchIssues([]string{"python", "中文"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = report.Execute(os.Stdout, res); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	fmt.Printf("q=%v", q)
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
