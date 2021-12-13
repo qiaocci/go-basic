a9c34c437f49a8f7e45a2dabdc1ac72c1df5aae7
qiaocc <qiaocco@gmail.com>
2021-12-13 17:00:28 +0800

    html模板

---
+++ 6-函数/go.mod
@@ -0,0 +1,8 @@
+module 6-func
+
+go 1.17
+
+require (
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20211205041911-012df41ee64c
+)
--- 7-指针/5-传值还是传引用/2-int指针.go
+++ 7-指针/5-传值还是传引用/2-int指针.go
@@ -7,6 +7,7 @@ import (
 // 我们传递一个int指针类型的参数，传递的其实是这个参数的一个副本
 func main() {
 	i := 10
+	fmt.Printf("原数据的指针是=%p\n", &i)
 	ip := &i
 	fmt.Printf("原始指针的地址是=%p\n", &ip)
 	modifyPtr(ip)
@@ -14,6 +15,6 @@ func main() {
 }
 
 func modifyPtr(ip *int) {
-	fmt.Printf("函数内部, 指针的地址是=%p\n", &ip)
+	fmt.Printf("函数里接收到的指针的地址是=%p\n", &ip)
 	*ip = 1
 }
